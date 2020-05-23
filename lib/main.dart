410d8651ceedcadec11c0479d5ea4513b3c5e80c
Guilherme Pejon <guilpejon@gmail.com>
2020-05-23 00:56:47 -0300

    First screen working

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:porissotoquebrado/screens/home_screen.dart';
-import 'package:porissotoquebrado/screens/loading_screen.dart';
-import 'components/product_card.dart';
-import 'package:porissotoquebrado/services/api.dart';
 
 // Uncomment lines 7 and 10 to view the visual layout at runtime.
 // import 'package:flutter/rendering.dart' show debugPaintSizeEnabled;
 
 void main() {
   // debugPaintSizeEnabled = true;
-//  Api api = Api("https://api.porissotoquebrado.com.br/api/v1/produtos");
-//  api.getData();
-
   runApp(MyApp());
 }
 
@@ -26,6 +20,5 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       home: HomeScreen(),
     );
-//        home: LoadingScreen(),);
   }
 }
--- lib/tabs/home_tab.dart
+++ lib/tabs/home_tab.dart
@@ -1,13 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:porissotoquebrado/services/api.dart';
 import 'package:http/http.dart' as http;
+import 'package:transparent_image/transparent_image.dart';
 
 class HomeTab extends StatelessWidget {
-  Future _fetchProducts() {
-    return Api("https://api.porissotoquebrado.com.br/api/v1/produtos")
-        .getData();
-  }
-
   @override
   Widget build(BuildContext context) {
     Widget _buildBodyBackground() => Container(
@@ -31,15 +28,20 @@ class HomeTab extends StatelessWidget {
             SliverAppBar(
               floating: true,
               snap: true,
-              backgroundColor: Colors.transparent,
+              backgroundColor: Color.fromARGB(255, 250, 129, 96),
               elevation: 0.0,
               flexibleSpace: FlexibleSpaceBar(
-                title: const Text("Produtos"),
+                title: const Text(
+                  "PorIssoToQuebrado",
+                  style: TextStyle(color: Colors.white),
+                ),
                 centerTitle: true,
               ),
             ),
             FutureBuilder(
-              future: _fetchProducts(),
+              future:
+                  Api("https://api.porissotoquebrado.com.br/api/v1/produtos")
+                      .getData(),
               builder: (context, snapshot) {
                 if (!snapshot.hasData) {
                   return SliverToBoxAdapter(
@@ -52,13 +54,20 @@ class HomeTab extends StatelessWidget {
                     ),
                   );
                 } else {
-                  print(snapshot);
-                  return SliverToBoxAdapter(
-                    child: Container(
-                      height: 200.0,
-                      alignment: Alignment.center,
-                      child: Container(),
-                    ),
+                  return SliverStaggeredGrid.count(
+                    crossAxisCount: 2,
+                    mainAxisSpacing: 1.0,
+                    crossAxisSpacing: 1.0,
+                    staggeredTiles: snapshot.data.map<StaggeredTile>((product) {
+                      return StaggeredTile.count(2, 2);
+                    }).toList(),
+                    children: snapshot.data.map<FadeInImage>((product) {
+                      return FadeInImage.memoryNetwork(
+                        placeholder: kTransparentImage,
+                        image: product.thumbnail,
+                        fit: BoxFit.cover,
+                      );
+                    }).toList(),
                   );
                 }
               },
